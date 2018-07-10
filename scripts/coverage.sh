ad7199e492b073ba68db8f88671d87f49fe2fa9c
jmpmonteagudo <jmpmonteagudo@gmail.com>
2018-07-10 11:39:55 +0800

    fix: coverage test

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,18 +2,28 @@
 
 port=8555
 
-# Import common variables.
-. scripts/common.sh
+# Fix number of accounts as it does not affect coverage test results
+export NUM_ACCOUNTS=20
+
+cleanup() {
+  [ -f allFiredEvents ] && rm allFiredEvents
+  [ -h truffle/node_modules ] && rm truffle/node_modules
+  [ -h truffle/allFiredEvents ] && rm truffle/allFiredEvents
+  return 0
+}
 
 # Executes cleanup function at script exit.
 trap cleanup EXIT
 
-if testrpc_running $port; then
+nc -z localhost $port
+if [ $? -eq 0 ]; then
   echo "Using existing testrpc-sc instance"
 else
   echo "Starting testrpc-sc to generate coverage"
-  eval ./node_modules/.bin/testrpc-sc --port $port --gasLimit 0xfffffffffff "$accounts" -u 0 -u 1 > /dev/null & 
+  eval ./node_modules/.bin/testrpc-sc --port $port --gasLimit 0xfffffffffff --accounts $NUM_ACCOUNTS -u 0 -u 1 > /dev/null &
   testrpc_pid=$!
 fi
 
-SOLIDITY_COVERAGE=true solidity-coverage
+(cd truffle && ln -s ../node_modules)
+(cd truffle && ln -s ../allFiredEvents)
+(cd truffle && ../node_modules/.bin/solidity-coverage)
