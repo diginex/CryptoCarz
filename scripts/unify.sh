33fc6cfae9d51a2d414769a32ee8b5b5f8bedb5a
jmpmonteagudo <jmpmonteagudo@gmail.com>
2018-07-19 11:21:52 +0800

    feat: update code unifier

--- scripts/unify.sh
+++ scripts/unify.sh
@@ -12,6 +12,12 @@ unify() {
 echo "pragma solidity 0.4.23;" > Unified.sol
 
 unify ../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol
+unify ../node_modules/openzeppelin-solidity/contracts/AddressUtils.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Basic.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Receiver.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721BasicToken.sol
+unify ../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol
 unify ../truffle/contracts/CryptoCarzControl.sol
 unify ../truffle/contracts/CryptoCarzToken.sol
 unify ../truffle/contracts/CryptoCarzAuction.sol
