64d4989607063488008a9cc584e5e88a67733891
jmpmonteagudo <jmpmonteagudo@gmail.com>
2018-07-19 11:21:28 +0800

    test: fix coverage test

--- scripts/run-coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-docker build . -f Dockerfile -t cryptocarz/truffle
-docker run --rm --volume coverage:/code/truffle/coverage cryptocarz/truffle bash scripts/coverage.sh
